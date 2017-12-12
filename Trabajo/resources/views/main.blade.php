d7afeedded03cd71ab5ad8faece38e7f7bdf311f
ger <you@example.com>
2017-12-12 17:22:34 -0300

    carusel de imagenes

--- Trabajo/resources/views/main.blade.php
+++ Trabajo/resources/views/main.blade.php
@@ -51,6 +51,40 @@
 					</center>
 				</div>
 			</div>
+			<div class="slideshow-container">
+
+				<!-- Full-width images with number and caption text -->
+				<div class="mySlides fade">
+					<div class="numbertext">1 / 3</div>
+					<img src="./images/boy3.jpeg" style="width:100%">
+					<div class="text">Tu vieja Uno</div>
+				</div>
+
+				<div class="mySlides fade">
+					<div class="numbertext">2 / 3</div>
+					<img src="./images/chi3.jpeg" style="width:100%">
+					<div class="text">Tu vieja Dos</div>
+				</div>
+
+				<div class="mySlides fade">
+					<div class="numbertext">3 / 3</div>
+					<img src="./images/boy1.jpeg" style="width:100%">
+					<div class="text">Tu vieja Tres</div>
+				</div>
+
+				<!-- Next and previous buttons -->
+				<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+				<a class="next" onclick="plusSlides(1)">&#10095;</a>
+			</div>
+
+			<br>
+
+			<!-- The dots/circles -->
+			<div style="text-align:center">
+				<span class="dot" onclick="currentSlide(1)"></span>
+				<span class="dot" onclick="currentSlide(2)"></span>
+				<span class="dot" onclick="currentSlide(3)"></span>
+			</div>
 <br>
 <center>
 			<div class="container">
